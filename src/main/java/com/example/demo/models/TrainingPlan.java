2d4e36e4ae18c22626a68c12449db71fee1d699c
Nolan Whittaker <152032773+NolanWhittaker1@users.noreply.github.com>
2024-03-11 21:07:42 -0700

    Working
    - Delete functionality dosent work but the program works.

--- src/main/java/com/example/demo/models/TrainingPlan.java
+++ src/main/java/com/example/demo/models/TrainingPlan.java
@@ -16,8 +16,6 @@ public class TrainingPlan {
     private String description;
     private LocalDate startDate;
     private LocalDate endDate;
-    @Column(name = "is_deleted", nullable = true)
-    private boolean isDeleted;
 
 
     @ManyToOne(cascade = CascadeType.ALL)
@@ -58,14 +56,6 @@ public class TrainingPlan {
 
     public int getTpid() {
         return tpid;
-    }
-
-    public void delete() {
-        this.isDeleted = true;
-    }
-
-    public boolean isDeleted() {
-        return isDeleted;
     }
     
 }
